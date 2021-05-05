b1afe826ec5b0c7e47418649436f4ef8b9a8cdfa
Ozan <ozan28061@gmail.com>
2021-05-05 14:45:17 +0200

    first commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <a href="aboutus">About Us</a>
+            <a href="foryou">For you</a>
+            <a href="services">SERVICES</a>
+            <a href="blog">BLOG</a>
+            <a href="vlog">VLOG</a>
+            <a href="contact">CONTACT</a>
+        </div>
+    )
+}
+
+ export default  Navbar;
